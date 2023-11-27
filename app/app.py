b728db5256370cf54bee480f5f536cbea42f896d
Daniel Grundhoefer <daniel.grundhoefer@computacenter.com>
2023-11-27 16:42:43 +0100

    working flask

--- app/app.py
+++ app/app.py
@@ -3,9 +3,13 @@ import datetime
 
 app = Flask(__name__)
 
+@app.route('/healthz')
+def healthz():
+	return "OK"
+
 @app.route('/')
 def home():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
