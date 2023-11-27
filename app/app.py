c91309fee544180b0d75be34f2d8d83743b9840b
Daniel Grundhoefer <daniel.grundhoefer@computacenter.com>
2023-11-27 16:05:27 +0100

    enhance template

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True)
